a7fa1f9f5b5e2ef5439fd75e5bb8a0f49e83e128
smarthug <kirklayer@gmail.com>
2023-04-21 06:03:15 +0900

    img insert

--- src/pages/metamaskInstall.js
+++ src/pages/metamaskInstall.js
@@ -8,20 +8,21 @@ export const MetamaskInstall = () => {
   };
 
   return (
-    <Container maxWidth="sm" style={{ marginTop: "2rem" }}>
-      <Typography variant="h4" align="center" gutterBottom>
-        Install MetaMask to continue
-      </Typography>
-      <Typography variant="body1" align="center" gutterBottom>
-        MetaMask is a browser extension that allows you to interact with Ethereum-based decentralized applications (dApps) like ours.
-      </Typography>
-      <Typography variant="body1" align="center" gutterBottom>
-        If you don't already have it installed, please follow the link below to download and install it for your browser.
-      </Typography>
-      <Button variant="contained" color="primary" onClick={handleInstallMetamask} style={{ marginTop: "2rem" }}>
-        Install MetaMask
-      </Button>
-    </Container>
+    <Container maxWidth="sm" style={{ marginTop: "2rem", display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/3/36/MetaMask_Fox.svg" alt="MetaMask logo" style={{ width: "100%" }} />
+    <Typography variant="h4" align="center" gutterBottom>
+      Install MetaMask to continue
+    </Typography>
+    <Typography variant="body1" align="center" gutterBottom>
+      MetaMask is a browser extension that allows you to interact with Ethereum-based decentralized applications (dApps) like ours.
+    </Typography>
+    <Typography variant="body1" align="center" gutterBottom>
+      If you don't already have it installed, please follow the link below to download and install it for your browser.
+    </Typography>
+    <Button variant="contained" color="primary" onClick={handleInstallMetamask} style={{ marginTop: "2rem" }}>
+      Install MetaMask
+    </Button>
+  </Container>
   );
 };
 
